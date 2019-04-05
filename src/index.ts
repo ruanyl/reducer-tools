b6846797fac2911928857bfc06f6a4a4bd5adec0
ruanyl <ruanyu1@gmail.com>
2019-04-05 09:13:29 +0300

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 export interface AnyAction {
   type: string;
-  [key: string]: any;
 }
+
 export type State = any
-export type Reducer = (state: State, action: AnyAction) => State;
-export interface ReducerMap {
-  [key: string]: Reducer
+export type Reducer<S, A> = (state: S, action: A) => State;
+export type ReducerMap<A extends AnyAction, S = State> = {
+  [key in A['type']]?: (state: S, action: A) => State
 }
 type FunctionalReducer = (v: any) => (s: State) => State
 type TransformFunction = (a: AnyAction) => any
 
-export const createReducer = (initialState: State, reducerMap: ReducerMap) => {
-  return (state: State = initialState, action: AnyAction) => {
+export const createReducer = <T extends AnyAction, S = State>(initialState: S, reducerMap: ReducerMap<T, S>) => {
+  return (state: S = initialState, action: T) => {
     if (reducerMap.hasOwnProperty(action.type)) {
       return reducerMap[action.type](state, action)
     } else {
@@ -25,3 +25,26 @@ export const transformReducer = (transform: TransformFunction) => (reducer: Func
 export const propReducer = (p: string) => transformReducer(prop(p))
 export const payloadReducer = propReducer('payload')
 export const valueReducer = propReducer('value')
+
+interface LoginData {
+  email: string;
+  password: string;
+}
+
+type a = ReturnType<any>
+
+export interface UserData {
+  id: string;
+  email: string;
+  profilePicture: string;
+}
+
+export type LoginAction =
+  | { type: 'LOGIN_REQUEST'; value: LoginData }
+  | { type: 'LOGIN_SUCCESS'; value: UserData }
+  | { type: 'LOGIN_FAILED'; value: string };
+
+type c = ReducerMap<LoginAction>
+const cc: c = {
+  'LOGIN_FAILED': (s, a) => a.value,
+}
