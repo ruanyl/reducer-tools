df62d373b16f8f3f7e9bd2e4c437ea1da3f59cf5
ruanyl <ruanyu1@gmail.com>
2019-04-08 21:44:29 +0300

    added typedKeyMirror

---
+++ src/createReducer.ts
@@ -0,0 +1,18 @@
+export interface AnyAction {
+  type: string;
+}
+
+export type State = any
+export type ReducerMap<A extends AnyAction, S = State> = {
+  [P in A['type']]?: A extends { type: P } ? (state: S, action: A) => S : never
+}
+
+export const createReducer = <A extends AnyAction, S = State>(initialState: S, reducerMap: ReducerMap<A, S>) => {
+  return (state: S = initialState, action: A): S => {
+    if (reducerMap.hasOwnProperty(action.type)) {
+      return (reducerMap as any)[action.type](state, action)
+    } else {
+      return state
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,2 @@
-export interface AnyAction {
-  type: string;
-}
-
-export type State = any
-export type ReducerMap<A extends AnyAction, S = State> = {
-  [P in A['type']]?: A extends { type: P } ? (state: S, action: A) => State : never
-}
-
-export const createReducer = <A extends AnyAction, S = State>(initialState: S, reducerMap: ReducerMap<A, S>) => {
-  return (state: S = initialState, action: A): S => {
-    if (reducerMap.hasOwnProperty(action.type)) {
-      return (reducerMap as any)[action.type](state, action)
-    } else {
-      return state
-    }
-  }
-}
+export * from './createReducer'
+export * from './typedKeyMirror'
