0758cbfea098226527879063a823fb07685b80e4
Yulong Ruan <yruan@alpha-sense.com>
2019-05-02 14:55:09 +0300

    support custom separator

--- src/typedKeyMirror.ts
+++ src/typedKeyMirror.ts
@@ -2,8 +2,8 @@ type Mirrored<T extends Record<string, any>> = {
   [K in keyof T]: K
 }
 
-export const typedKeyMirror = <T extends Record<string, any>>(keyMap: T, namespace: string) => {
+export const typedKeyMirror = <T extends Record<string, any>>(keyMap: T, namespace: string, separator: string = '_') => {
   const keyMirrored: Mirrored<T> = {} as Mirrored<T>
-  Object.keys(keyMap).forEach(k => keyMirrored[k] = `${namespace}_${k}`)
+  Object.keys(keyMap).forEach(k => keyMirrored[k] = `${namespace}${separator}${k}`)
   return keyMirrored
 }
